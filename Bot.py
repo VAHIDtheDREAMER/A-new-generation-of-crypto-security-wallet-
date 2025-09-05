db78de91361743abbf0d69d27b6b5998077eaa8e
VAHIDtheDREAMER <vahidbeykzadeh@gmail.com>
2025-09-05 05:19:47 +0330

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -3,7 +3,7 @@ from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
 # توکن از متغیر محیطی (Env Var) گرفته میشه
-TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+TOKEN = os.getenv("8293627914:AAHsIddC1XExdUo2_4KOacwJVj17SLQBkBwN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("سلام! 🤖 من ربات VaultX هستم. آماده‌ای برای شروع مزایده؟")
